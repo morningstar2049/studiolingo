7e7654e2c9925d11ce0e06a68e359d4a83866cec
Vakho <vakho.daraselia@gmail.com>
2023-08-08 15:09:53 +0400

    header and nav for desktop done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 "use client";
 import Image from "next/image";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useMediaQuery } from "react-responsive";
-function Navbar() {
-  const isNotMobile = useMediaQuery({ query: "(min-width: 640px)" });
+import dynamic from "next/dynamic";
+const MediaQuery = dynamic(() => import("react-responsive"), { ssr: false });
 
+function Navbar() {
   return (
     <>
-      {isNotMobile && (
+      <MediaQuery minWidth={640}>
         <header className="bg-white h-[110px] flex justify-around items-center">
           <Image
             src="/lingo-logo-main.png"
@@ -23,7 +23,7 @@ function Navbar() {
               height={50}
             />
             <Image
-              src="/icons8-instagram.svg"
+              src="/instagram-green.svg"
               alt="instagram-green"
               width={50}
               height={50}
@@ -35,29 +35,35 @@ function Navbar() {
               height={50}
             />
             <Image
-              src="/icons8-linkedin.svg"
+              src="/linkedin-green.svg"
               alt="linkedin-green"
               width={50}
               height={50}
             />
           </div>
         </header>
-      )}
-      <nav className="bg-white sm:bg-lingo-green h-[80px] flex justify-between items-center shadow p-10">
-        <div>კურსები</div>
-        <div>კორპორაციული</div>
-        <div>ჩვენი გუნდი</div>
-        <div>შეფასებები</div>
+      </MediaQuery>
 
-        {!isNotMobile && (
+      <nav
+        style={{ fontFeatureSettings: "'case' on" }}
+        className="h-14 bg-white sm:bg-lingo-green font-bold text-[white] flex sm:justify-around justify-between items-center shadow"
+      >
+        <div className="flex justify-evenly w-7/12 ml-[-200px]">
+          <div>კურსები</div>
+          <div>კორპორაციული</div>
+          <div>ჩვენი გუნდი</div>
+          <div>შეფასებები</div>
+        </div>
+
+        <MediaQuery maxWidth={640}>
           <Image
             src="/lingo-logo-main.png"
             alt="lingo-logo"
             width={120}
             height={120}
           />
-        )}
-        {!isNotMobile && <GiHamburgerMenu color="#2f9e4d" fontSize="20px" />}
+          <GiHamburgerMenu color="#2f9e4d" fontSize="20px" />
+        </MediaQuery>
       </nav>
     </>
   );
