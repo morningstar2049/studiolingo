5e8cb868eeabb3f1f8d6a3d67eea01a6789fb130
Vakho <vakho.daraselia@gmail.com>
2023-09-05 14:37:00 +0400

    fix IlikoMessage picture height and scroll margin on big screens

--- src/components/IlikoMessage.tsx
+++ src/components/IlikoMessage.tsx
@@ -3,10 +3,7 @@ import Image from "next/image";
 export default function IlikoMessage() {
   return (
     <>
-      <div
-        id="courses"
-        className="relative sm:w-1/2 sm:h-[700px] w-[100%] h-[370px] -z-10 self-start"
-      >
+      <div className="relative sm:w-1/2 sm:h-[600px] w-[100%] h-[370px] -z-10 self-start">
         <Image
           src="/iliko.svg"
           alt="iliko"
