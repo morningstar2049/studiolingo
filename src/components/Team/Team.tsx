1e7ade2b6b9ef48f9d46e1c750ef022d80c4fe8a
Vakho <vakho.daraselia@gmail.com>
2023-09-23 17:17:11 +0400

    add id and scroll margin to team section

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -2,9 +2,9 @@ import TeamBanner from "./TeamBanner";
 
 function Team() {
   return (
-    <>
+    <div id="team" className="scroll-m-[160px] sm:scroll-m-[200px]">
       <TeamBanner />
-    </>
+    </div>
   );
 }
 
