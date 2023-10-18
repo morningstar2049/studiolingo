e458def47c5688992d49dfa37b5ce4e5e6a72439
Vakho <vakho.daraselia@gmail.com>
2023-10-18 15:57:57 +0400

    add youtube video to courses section, make reusable YoutubeVideo component

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,14 +1,19 @@
 import CoursesAccordion from "./CoursesAccordion";
 import IlikoMessage from "../IlikoMessage";
+import YoutubeVideo from "../YoutubeVideo";
 
 function Courses() {
   return (
-    <div
-      id="courses"
-      className="scroll-m-[85px] sm:items-center sm:flex sm:scroll-m-[145px]"
-    >
-      <IlikoMessage />
-      <CoursesAccordion />
+    <div className="flex flex-col gap-48">
+      <div
+        id="courses"
+        className="scroll-m-[85px] sm:items-center sm:flex sm:scroll-m-[145px]"
+      >
+        <IlikoMessage />
+        <CoursesAccordion />
+      </div>
+
+      <YoutubeVideo src="https://www.youtube.com/embed/cFNGpLMF5nQ" />
     </div>
   );
 }
