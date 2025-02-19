1f8b36036bb8b80b629dd659efd93ccc7bb3c9d7
Vakho <vakho.daraselia@gmail.com>
2025-02-19 14:19:19 +0400

    add not available text for deleted vacancies on career page

--- src/app/career/[positionId]/page.tsx
+++ src/app/career/[positionId]/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import PositionDetails, { TPositionKey } from "./components/PositionDetails";
 import { positionData } from "./positionData";
+import { vacancies } from "../components/VacanciesList";
 
 export async function generateMetadata({
   params,
@@ -16,9 +17,19 @@ export async function generateMetadata({
 }
 
 function page({ params }: { params: { positionId: TPositionKey } }) {
+  const isPositionOpen = !!vacancies.filter(
+    (item) => item.route === params.positionId
+  ).length;
+
   return (
     <div className="flex justify-center p-5 sm:p-9">
-      <PositionDetails positionKey={params.positionId} />
+      {isPositionOpen ? (
+        <PositionDetails positionKey={params.positionId} />
+      ) : (
+        <h1 className="text-lingo-green font-bold text-xl text-center h-[calc(100vh-200px)] flex items-center justify-center">
+          ამჟამად მიმდინარე ვაკანსია დახურულია
+        </h1>
+      )}
     </div>
   );
 }
--- src/app/career/components/VacanciesList.tsx
+++ src/app/career/components/VacanciesList.tsx
@@ -1,13 +1,13 @@
 import VacanciesListItem, { VacanciesListItemProps } from "./VacanciesListItem";
 
-const vacancies: VacanciesListItemProps[] = [
-  {
-    position: "ინგლისურის მასწავლებელი",
-    location: "თბილისი",
-    time: "სრული",
-    salary: "კონკურენტული",
-    route: "englishTeacher",
-  },
+export const vacancies: VacanciesListItemProps[] = [
+  // {
+  //   position: "ინგლისურის მასწავლებელი",
+  //   location: "თბილისი",
+  //   time: "სრული",
+  //   salary: "კონკურენტული",
+  //   route: "englishTeacher",
+  // },
   // {
   //   position: "გერმანულის მასწავლებელი",
   //   location: "თბილისი",
@@ -41,16 +41,22 @@ const vacancies: VacanciesListItemProps[] = [
 function VacanciesList() {
   return (
     <div className="flex flex-col items-center justify-around gap-10">
-      {vacancies.map((vacancy) => (
-        <VacanciesListItem
-          key={vacancy.position}
-          position={vacancy.position}
-          location={vacancy.location}
-          time={vacancy.time}
-          salary={vacancy.salary}
-          route={vacancy.route}
-        />
-      ))}
+      {!!vacancies.length ? (
+        vacancies.map((vacancy) => (
+          <VacanciesListItem
+            key={vacancy.position}
+            position={vacancy.position}
+            location={vacancy.location}
+            time={vacancy.time}
+            salary={vacancy.salary}
+            route={vacancy.route}
+          />
+        ))
+      ) : (
+        <h1 className="text-lingo-green font-bold text-xl text-center h-[calc(100vh-200px)] flex items-center justify-center">
+          ამჟამად ყველა ვაკანსია დახურულია
+        </h1>
+      )}
     </div>
   );
 }
